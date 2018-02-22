e65c4228afa954cc6f9ea6695cb1600673769c60
Atomnus <33552217+Atomnus@users.noreply.github.com>
2018-02-22 09:39:22 -0500

    Delete puzzle.js

--- puzzle.js
+++
@@ -1,8 +0,0 @@
-function unhide(id) {
-  var x = document.getElementById(id);
-    if (!x.style.display || x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
